4ca24f3d91a5dd5430ba60aafbef7093568f0c9a
aaronconvery3 <aaronconvery44@gmail.com>
2026-03-16 20:57:01 -0400

    commit

---
+++ Lab4/Main.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Unsorted Array ---------------------------------------------------");
+        ArrayList<Integer> integerList = Lab4.getList();
+        Lab4.outputList(integerList);
+
+        System.out.println("\n\nBubble sort results ----------------------------------------------");
+
+        long startBubble = System.nanoTime();
+        ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(new ArrayList<>(integerList));
+        long endBubble = System.nanoTime();
+
+        Lab4.outputList(bubbleSortedList);
+        System.out.println("\nBubble sort time: " + (endBubble - startBubble) + " nanoseconds");
+
+        System.out.println("\n\nInsertion sort results -------------------------------------------");
+
+        long startInsertion = System.nanoTime();
+        ArrayList<Integer> insertionSortedList = Lab4.insertionSort(new ArrayList<>(integerList));
+        long endInsertion = System.nanoTime();
+
+        Lab4.outputList(insertionSortedList);
+        System.out.println("\nInsertion sort time: " + (endInsertion - startInsertion) + " nanoseconds");
+    }
+}
+
+class Lab4 {
+
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
+
+        for (int i = 1; i < integerList.size(); i++) {
+
+            int current = integerList.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && integerList.get(j) > current) {
+                integerList.set(j + 1, integerList.get(j));
+                j--;
+            }
+
+            integerList.set(j + 1, current);
+        }
+
+        return integerList;
+    }
+
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
+
+        for (int i = 0; i < integerList.size() - 1; i++) {
+
+            for (int j = 0; j < integerList.size() - i - 1; j++) {
+
+                if (integerList.get(j) > integerList.get(j + 1)) {
+
+                    int temp = integerList.get(j);
+                    integerList.set(j, integerList.get(j + 1));
+                    integerList.set(j + 1, temp);
+                }
+            }
+        }
+
+        return integerList;
+    }
+
+    public static ArrayList<Integer> getList() {
+
+        ArrayList<Integer> integerList = new ArrayList<>();
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
+
+            while ((line = br.readLine()) != null) {
+                integerList.add(Integer.parseInt(line));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return integerList;
+    }
+
+    public static void outputList(ArrayList<Integer> integerList) {
+
+        for (int i = 0; i < integerList.size(); i++) {
+            System.out.print(integerList.get(i) + " ");
+        }
+    }
+}
